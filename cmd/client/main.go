e98dcc82cf472cecda8a4d0f52107b8421272c0c
misterlister <haydenglister@gmail.com>
2025-10-27 12:51:06 -0700

    Created initial client setup

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connection successful!")
+
+	username, err := gamelogic.ClientWelcome()
+
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		routing.Transient,
+	)
+
+	if err != nil {
+		fmt.Printf("error: %v", err)
+		return
+	}
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("\nShutting down connection...")
 }
