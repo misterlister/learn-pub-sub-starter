3814f2158b5cd4ac0f9c96df9075d735eed564f8
misterlister <haydenglister@gmail.com>
2025-10-29 13:30:34 -0700

    Added newlines to error messages

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -33,14 +33,14 @@ func main() {
 	)
 
 	if err != nil {
-		fmt.Printf("error: could not connect to game logs queue. - %v", err)
+		fmt.Printf("error: could not connect to game logs queue. - %v\n", err)
 		return
 	}
 
 	connChan, err := conn.Channel()
 
 	if err != nil {
-		fmt.Printf("error: could not create connection channel. - %v", err)
+		fmt.Printf("error: could not create connection channel. - %v\n", err)
 		return
 	}
 
@@ -63,7 +63,7 @@ func main() {
 			err = pubsub.PublishJSON(connChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
 
 			if err != nil {
-				fmt.Printf("error: could not publish pause command. - %v", err)
+				fmt.Printf("error: could not publish pause command. - %v\n", err)
 				continue
 			}
 			fmt.Println("Game paused.")
@@ -74,7 +74,7 @@ func main() {
 			err = pubsub.PublishJSON(connChan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
 
 			if err != nil {
-				fmt.Printf("error: could not publish resume command. - %v", err)
+				fmt.Printf("error: could not publish resume command. - %v\n", err)
 				continue
 			}
 			fmt.Println("Game resumed.")
